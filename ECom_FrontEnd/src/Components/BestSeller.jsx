0680a0d76aeb58f6e2b5eaa8dd53446dc60f4061
Gajapathi K <gajapathi787@gmail.com>
2025-04-19 23:23:43 +0530

    submit

---
+++ ECom_FrontEnd/src/Components/BestSeller.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BestSeller = () => {
+  return (
+    <div className='mt-16'>
+
+        <p className='text-2xl md:text-3xl font-medium'>Best Seller</p>
+      
+    </div>
+  )
+}
+
+export default BestSeller
