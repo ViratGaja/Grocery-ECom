a161ec79ccac51fdae215f3aa331b0e824d9226a
Gajapathi K <gajapathi787@gmail.com>
2025-04-27 23:21:09 +0530

    update

--- ECom_FrontEnd/src/Pages/Home.jsx
+++ ECom_FrontEnd/src/Pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import MainBanner from '../Components/MainBanner'
 import Categories from '../Components/Categories'
 import BestSeller from '../Components/BestSeller'
+import BottomBanner from '../Components/BottomBanner'
+import NewsLetter from '../Components/NewsLetter'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
         <MainBanner/>
         <Categories/>
         <BestSeller/>
+        <BottomBanner/>
+        <NewsLetter/>
       
     </div>
   )
